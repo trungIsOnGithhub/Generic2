aa6a562bd5ab999409d272431b63a5ca773058f7
trungvuive <trung150502@gmail.com>
2024-01-10 15:40:43 +0700

    test pass stage 2

--- app/main.go
+++ app/main.go
@@ -12,7 +12,11 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 	
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 
 	if err != nil {
 		fmt.Printf("Err: %v", err)

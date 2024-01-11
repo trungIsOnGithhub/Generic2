bc4cf719653eb3a9f3aeaedda737a0b064966291
trungvuive <trung150502@gmail.com>
2024-01-11 14:46:19 +0700

    add logic to copying command to container

--- app/main.go
+++ app/main.go
@@ -15,50 +15,77 @@ func (nullReader) Read(p []byte) (int, error) {
 	return len(p), nil
 }
 
+// copy file to sandbox keeping permission
+func copy_file_to_jail(src_exe_path string, jailed_command_path string) error {
+	src_command_fd, err := os.Open(src_exe_path)
+
+	if err != nil { return err }
+
+	defer src_command_fd.Close()
+
+	jailed_command_fd, err := os.Create(jailed_command_path)
+
+	if err != nil { return err }
+
+	defer jailed_command_fd.Close()
+
+	src_command_fd_info, err := src_command_fd.Stat()
+
+	if err != nil { return err }
+
+	// change jailed command to exactly the mod of
+	jailed_command_fd.Chmod(src_command_fd_info.Mode())
+
+	_, err = io.Copy(jailed_command_fd, src_command_fd)
+
+	return err
+}
+
 func sandbox_jail(command string, sandbox_path string) (jailed_command string, err error) {
 	// search command executeable by looking at PATH
 	src_exe_path, err := exec.LookPath(command)
-
 	if err != nil { return "", err }
 
+	// get command path
 	jailed_command_path := filepath.Join(sandbox_path, src_exe_path)
 
+	// make a copy dir in snadbox
 	err = os.MkdirAll(filepath.Dir(jailed_command_path), 0755)
+	if err != nil { return "", err }
+
+	// copy executable to sandbox
+	err = copy_file_to_jail(src_exe_path, jailed_command_path)
 
 	if err != nil { return "", err }
 
-	err = copy_file_to_jail(srcCommandPath, sandboxedCommandPath)
-
-	if err != nil { return "", err }
-
-	return srcCommandPath, nil
+	return src_exe_path, nil
 }
 
 func run_container(command string, args []string, sandbox_path string) {
 	cmd := exec.Command(command, args...)
 
 	cmd.Stdin = nullReader{}
-
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
-	err := syscall.Chroot(sandbox_path)
+	// err := cmd.Run()
+	// if err != nil {
+	// 	fmt.Printf("Unable to Start Command1\n")
+	// }
 
+	err := syscall.Chroot(sandbox_path)
 	if err != nil {
-		fmt.Printf("Unable to sandbox command")
+		fmt.Printf("Unable to sandbox command!\n")
 		os.Exit(1)
 	}
 
 	err = syscall.Chdir("/")
-
 	if err != nil {
 		fmt.Printf("Unable to chdir to sandbox root!\n")
 		os.Exit(1)
 	}
 
 	err = cmd.Run()
-
 	if err != nil {
 		fmt.Printf("Error Running Container!\n")
 		os.Exit(cmd.ProcessState.ExitCode())

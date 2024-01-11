b94a418546a2b2bedd0ee2a5704e67d52d0fe98b
trungvuive <trung150502@gmail.com>
2024-01-11 13:52:55 +0700

    add jailing sanbox logic function

--- app/main.go
+++ app/main.go
@@ -15,7 +15,26 @@ func (nullReader) Read(p []byte) (int, error) {
 	return len(p), nil
 }
 
-func runContainer(command string, args []string, sandbox_path string) {
+func sandbox_jail(command string, sandbox_path string) (jailed_command string, err error) {
+	// search command executeable by looking at PATH
+	src_exe_path, err := exec.LookPath(command)
+
+	if err != nil { return "", err }
+
+	jailed_command_path := filepath.Join(sandbox_path, src_exe_path)
+
+	err = os.MkdirAll(filepath.Dir(jailed_command_path), 0755)
+
+	if err != nil { return "", err }
+
+	err = copy_file_to_jail(srcCommandPath, sandboxedCommandPath)
+
+	if err != nil { return "", err }
+
+	return srcCommandPath, nil
+}
+
+func run_container(command string, args []string, sandbox_path string) {
 	cmd := exec.Command(command, args...)
 
 	cmd.Stdin = nullReader{}
@@ -34,14 +53,14 @@ func runContainer(command string, args []string, sandbox_path string) {
 	err = syscall.Chdir("/")
 
 	if err != nil {
-		fmt.Printf("Unable to chdir to sandbox root")
+		fmt.Printf("Unable to chdir to sandbox root!\n")
 		os.Exit(1)
 	}
 
 	err = cmd.Run()
 
 	if err != nil {
-		fmt.Printf("Error Running Container")
+		fmt.Printf("Error Running Container!\n")
 		os.Exit(cmd.ProcessState.ExitCode())
 	}
 }
@@ -55,11 +74,18 @@ func main() {
 
 	// fmt.Printf("argument 3: %s\n", command)
 	// fmt.Printf("remained arguments : %s\n", args)
-	
+
 	jail_dir, err := os.MkdirTemp("/tmp", "my-docker")
 
 	if err != nil {
-		fmt.Printf("Error Creating Jail Directory")
+		fmt.Printf("Error Creating Jail Director!\n")
+		os.Exit(1)
+	}
+
+	jailed_command, err := sandbox_jail(command, jail_dir)
+
+	if err != nil {
+		fmt.Printf("Error jailing command!\n")
 		os.Exit(1)
 	}
 

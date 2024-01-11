f0dc9ac4ba57e7bfd39582820c9a436b4586f2b7
trungvuive <trung150502@gmail.com>
2024-01-11 11:59:39 +0700

    add run command in chroot mode

--- app/main.go
+++ app/main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
-	// 	"fmt"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 )
+
+type nullReader struct {}
+
+func runContainer(command string, args []string, sandboxPath string) {
+	cmd := exec.Command(command, args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	err := syscall.Chroot(sandboxPath)
+
+	if err != nil {
+		fmt.Printf("Unable to sandbox command")
+		os.Exit(1)
+	}
+
+	err = syscall.Chdir("/")
+
+	if err != nil {
+		fmt.Printf("Unable to chdir to sandbox root")
+		os.Exit(1)
+	}
+
+	err = cmd.Run()
+
+	if err != nil {
+		fmt.Printf("Error Running Container")
+
+		os.Exit(cmd.ProcessState.ExitCode())
+	}
+}
 
 // Usage: my_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
